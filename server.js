6891d86235d807b6d553bf166de7233cdf972297
eduardofleck <edusfleck@gmail.com>
2023-10-30 15:18:08 +0100

    First commit, express server using env variables

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { postgraphile } = require("postgraphile");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+const options = {
+  origin: process.env.CORS,
+};
+
+app.use(cors(options));
+
+// Enable pre-flight requests for all routes
+app.options("*", cors(options));
+
+app.use(
+  postgraphile(process.env.DATABASE_URL, "public", {
+    watchPg: true,
+    graphiql: true,
+    enhanceGraphiql: true,
+    disableDefaultMutations: true,
+    appendPlugins: [require("@graphile-contrib/pg-simplify-inflector")],
+    showErrorStack: "json",
+    extendedErrors: ["hint", "detail", "errcode"],
+  })
+);
+
+app.listen(process.env.PORT || 5000);
